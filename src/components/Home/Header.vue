a90285c437e1b3d1d180bae774c831d9114cae2a
Robert Kotcher <rkotcher@Roberts-MacBook-Pro.local>
2024-06-19 11:59:20 +0200

    Update homepage UI - cleaner, new picture, external links

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -2,61 +2,31 @@
   <div id="header">
     <div class="chip-container-small" v-if="tagStore.state.activeTag">
       <a href="/" class="header-title-small">Robert Kotcher</a>
-      <Chip small primary tag="learning" text="learning 📄" :selected="isActiveTag('learning')"/>
+      <Chip small primary tag="learning" text="learning" :selected="isActiveTag('learning')"/>
       <Chip small primary tag="industry" text="industry" :selected="isActiveTag('industry')"/>
-      <Chip small primary tag="languages" text="idiomas / 语言" :selected="isActiveTag('languages')"/>
+      <Chip small primary tag="travel" text="travel" :selected="isActiveTag('languages')"/>
       <Chip small secondary tag="old" text="old" :selected="isActiveTag('old')"/>
     </div>
     <div class="header-container" v-if="!tagStore.state.activeTag">
-        <div class="left-panel"></div>
-        <div class="right-panel animation-panel-second">
-          <table class="intro-table">
-            <tr>
-              <td class="outer-col emoji-col"></td>
-              <td class="outer-col"><div class="header-title">Robert Kotcher</div></td>
-            </tr>
-          </table>
-        </div>
-    </div>
-    <div class="header-container" v-if="!tagStore.state.activeTag">
-      <div class="left-panel image-panel animation-panel-first"></div>
-      <div class="right-panel animation-panel-second">
-        <table class="intro-table">
-          <tr>
-            <td class="upper-col emoji-col">👋</td>
-            <td class="upper-col">Welcome to my website!</td>
-          </tr>
-          <tr>
-            <td class="inner-col emoji-col">🌱</td>
-            <td class="inner-col">My favorite things are <a href="/?tag=travel" target="_blank">travelling</a>, <a href="https://robertkotcher.github.io/blog" target="_blank">learning languages</a>, and <a href="/?tag=learning" target="_blank">learning new technical skills</a>.</td>
-          </tr>
-          <tr>
-            <td class="inner-col emoji-col">🎓</td>
-            <td class="inner-col">I'm a Carnegie Mellon alumni (computer science and music).</td>
-          </tr>          
-          <tr>
-            <td class="outer-col emoji-col">💼</td>
-            <td class="outer-col">You can learn more about my work experiences <a href="/?tag=industry" target="_blank">here</a>.</td>
-          </tr>
-        </table>
+      <div class="image-panel animation-panel-first">
+        <img src="../../assets/home-minified.jpg"/>
       </div>
-    </div>
-    <div class="header-container" v-if="!tagStore.state.activeTag">
-      <div class="left-panel"></div>
-      <div class="right-panel animation-panel-third">
-        <table class="intro-table">
-          <tr>
-            <td class="outer-col emoji-col"></td>
-            <td class="outer-col">
-              <div class="chip-container">
-                <Chip primary tag="learning" text="learning 📄"/>
-                <Chip primary tag="industry" text="industry"/>
-                <Chip primary tag="languages" text="idiomas / 语言"/>
-                <Chip secondary tag="old" text="old"/>
-              </div>
-            </td>
-          </tr>
-        </table>
+      <div class="intro-panel animation-panel-second">
+        <div class="header-title">Robert Kotcher</div>
+        <p class="header-text">
+          Computers, travel, sushi.
+        </p>
+        <div class="link-container">
+          <a target="_blank" href="https://huggingface.co/rkotcher"><img src="../../assets/hf.png" class="header-link"/></a>
+          <a target="_blank" href="https://github.com/robertkotcher"><img src="../../assets/gh.png" class="header-link"/></a>
+          <a target="_blank" href="https://www.linkedin.com/in/robert-kotcher-639105196/"><img src="../../assets/li.png" class="header-link"/></a>
+        </div>
+        <div class="chip-container">
+          <Chip primary tag="learning" text="learning"/>
+          <Chip primary tag="industry" text="industry"/>
+          <Chip primary tag="travel" text="travel"/>
+          <Chip secondary tag="old" text="old"/>
+        </div>
       </div>
     </div>
   </div>
@@ -127,14 +97,13 @@
   }
 
   .chip-container {
-    flex: 1;
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+    justify-content: center;
     align-items: center;
     margin: 12px 0;
     width: 100%;
-    height: 50px;
   }
 
   .chip-container-small {
@@ -148,6 +117,20 @@
     height: 38px;
   }
 
+  .link-container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 40px;
+  }
+
+  .link-container > a {
+    margin: 0 8px;
+  }
+
   @media only screen and (max-width: 550px) {
     .left-panel {
       display: none;
@@ -155,15 +138,40 @@
   }
 
   .image-panel {
-    background: url(../../assets/profile.jpg);
-    background-size: cover;
-    background-position: 50% 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .image-panel img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0.5);
+    min-width: 100%;
+    min-height: 100%;
+    object-fit: cover;
+  }
+
+  .intro-panel {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    top: 0;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
   }
 
   .header-title {
-    margin-bottom: 12px;
-    white-space: nowrap;
-    font-size: 38px;
+    text-align: center;
+    font-size: 50px;
     font-weight: 500;
     line-height: 1;
   }
@@ -176,6 +184,24 @@
     font-weight: 500;
     line-height: 1;
   }  
+
+  .header-text {
+    margin: 16px 0;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    font-size: 20px;
+    text-align: center;
+    padding: 0 8px;
+  }
+
+  .header-link {
+    width: 32px;
+    height: 32px;
+  }
 
   .right-panel {
     flex: 1;
